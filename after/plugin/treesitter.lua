5b00760f7ce592cfa4519f1ca5458cec4981c68f
UnknownGravy <abdullahbinnur89@gmail.com>
2023-06-10 16:07:54 +0600

    transparency and lsp zero

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "c", "lua"},
+  ensure_installed = { "python", "c", "lua", "rust", "go"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -12,17 +12,37 @@ return require('packer').startup(function(use)
     use 'tpope/vim-fugitive'
     use 'nvim-treesitter/nvim-treesitter'
 
+
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
+    use "EdenEast/nightfox.nvim" -- Packer
+    use "nvim-tree/nvim-tree.lua"
+    use "xiyaowong/transparent.nvim"
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
 end)
 
